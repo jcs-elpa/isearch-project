1077b395e1c34a734120faf1f3a7feae6dc4d9c5
JenChieh <jcs090218@gmail.com>
2020-09-07 22:53:47 +0800

    Bump version.

--- isearch-project.el
+++ isearch-project.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Incremental search through the whole project.
 ;; Keyword: convenience, search
-;; Version: 0.2.5
+;; Version: 0.2.6
 ;; Package-Requires: ((emacs "26.1") (f "0.20.0"))
 ;; URL: https://github.com/jcs-elpa/isearch-project
 
